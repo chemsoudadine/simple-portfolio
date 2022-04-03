2120e8cdf1ce19786e6b1ca290306f39d15c7120
Chemsou <chemsou.dadine@gmail.com>
2022-04-03 18:14:11 +0100

    Simple Portfolio

---
+++ js/MultiText.js
@@ -0,0 +1,8 @@
+// https://mattboldt.com/demos/typed-js/
+
+var typing=new Typed(".MultiText", {
+    strings: [ "Web Designer", "Freelancer", "Graphics Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+});
